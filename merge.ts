07bc2826658f218c1c689669415e5528c4715be1
Giovanni Longobucco <giovanni.longobucco96@gmail.com>
2023-06-17 17:46:49 +0200

    mege exercise

--- merge.ts
+++ merge.ts
@@ -3,3 +3,15 @@
 //Crea due osservabili, uno che emetta una sequenza di numeri pari e l'altro che emetta una sequenza di numeri dispari.
 //Combina i due osservabili utilizzando l'operatore merge.
 //Sottoscriviti all'osservabile combinato e stampa ogni numero emesso nella console.
+import { merge, of } from "rxjs"
+
+
+
+const evenNumbers$ = of(2,4,6,8)
+
+// Osservabile per numeri dispari
+const oddNumbers$ = of(1,3,5,7)
+
+const combine = merge(evenNumbers$, oddNumbers$)
+
+combine.subscribe(el => console.log(el))
